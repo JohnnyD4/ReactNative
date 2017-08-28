c3a6ece4e108fc69702c5ada665f0db9139a37a5
John Davis <johnsdavis95@gmail.com>
2017-08-28 15:45:59 -0400

    testing native waters

--- practice/components/App.js
+++ practice/components/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Open up App.js to start working on your app!</Text>
         <Text style={styles.text}>Hello!df</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -20,9 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
   text: {
-
-    color: 'green',
-  },
+    flex: 2,
+    backgroundColor: '#222'
+  }
 });
