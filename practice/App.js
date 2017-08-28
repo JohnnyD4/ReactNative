69bed22e6a2ecb6a1d696863c258f79c50280eeb
John Davis <johnsdavis95@gmail.com>
2017-08-28 17:46:58 -0400

    adding tab

--- practice/App.js
+++ practice/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Button from 'react-native-button';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
 
 export default class App extends React.Component {
+    
+    state = {
+        index: 0,
+        routes: [
+            { key: '1', title: 'First' },
+            { key: '2', title: 'Second' },
+        ],
+    };
+
+    _handleIndexChange = index => this.setState({ index });
+ 
+     _renderHeader = props => <TabBar {...props} />;
+ 
+    _renderScene = SceneMap({
+        '1': FirstRoute,
+        '2': SecondRoute,
+    });
+    
     _onPressButton() {
     Alert.alert("hello")
 
@@ -11,7 +33,7 @@ export default class App extends React.Component {
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={this._onPressButton}>
-                <Text>Open up App.js to start woffdfgcvbrking on your app!</Text>
+                <Text>Open up App.js to starghfght woffdfgcvbrking on your app!</Text>
                 <Text style={styles.text}>Hello!df</Text>
             </TouchableOpacity>
             <Button
